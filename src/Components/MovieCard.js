7959a922d02bb7e1a1a25a0c8e63cc10f536314c
Gilblasse <gilblasse@gmail.com>
2020-07-30 02:01:57 -0400

    created MovieCard

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Context from "../Config/Context";
+import { withRouter } from "react-router-dom";
+import { Skeleton } from "@material-ui/lab";
+
+function MovieCard(props) {
+  const {
+    movie: { poster_path, title },
+  } = props;
+  const useStyles = makeStyles({
+    root: {
+      width: 250,
+      marginTop: 50,
+    },
+    media: {
+      height: 360,
+    },
+  });
+
+  const classes = useStyles();
+
+  const handleClick = (show, movie) => {
+    show(movie);
+    props.history.push(`/movies/${movie.title}`);
+  };
+
+  return (
+    <Context>
+      {({ showMovie }) => {
+        return !props.movie || !showMovie ? (
+          <div style={{ marginBottom: 40 }}>
+            <Skeleton variant="rect" width={210} height={318} />
+            <Skeleton variant="text" />
+          </div>
+        ) : (
+          <div onClick={() => handleClick(showMovie, props.movie)}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                  title={title}
+                />
+
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="body1"
+                    align="center"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <strong>
+                      {title.length > 17 ? `${title.slice(0, 18)} ...` : title}
+                    </strong>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </div>
+        );
+      }}
+    </Context>
+  );
+}
+
+export default withRouter(MovieCard);
