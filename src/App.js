a42ece253a381ccb8ce7d1e934a142888f149585
Gilblasse <gilblasse@gmail.com>
2020-07-30 01:59:54 -0400

    perparing react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {CssBaseline} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import SideBar from './Components/SideBar';
+import NavBar from './Containers/NavBar';
+import Main from './Containers/MainContainer';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
-function App() {
+
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: theme.mixins.toolbar,
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+
+
+function App(props) {
+  const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = useState(false);
+  
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
+      
+      
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <SideBar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
+      
+      </nav>
+     
+     
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Main/>
+      </main>
+
     </div>
+    </Router>
   );
 }
 
-export default App;
+export default App
